948bdd45886f9fdb81306669ff4be2183b0837e1
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2025-06-11 21:48:10 +0200

    Character can jump

--- classes/character.class.js
+++ classes/character.class.js
@@ -30,62 +30,82 @@ class Character extends MovableObject {
         '../assets/images/character/sliding/Sliding_002.png',
         '../assets/images/character/sliding/Sliding_003.png'
     ]
+
+    images_jumping = [
+       ' assets/images/character/jump/Jump_000.png',
+       ' assets/images/character/jump/Jump_001.png',
+       ' assets/images/character/jump/Jump_002.png',
+       ' assets/images/character/jump/Jump_003.png',
+       ' assets/images/character/jump/Jump_004.png'
+    ];
+
+    images_idle = [
+        'assets/images/character/idle/Idle_000.png',
+        'assets/images/character/idle/Idle_001.png',
+        'assets/images/character/idle/Idle_002.png',
+        'assets/images/character/idle/Idle_003.png',
+        'assets/images/character/idle/Idle_004.png',
+        'assets/images/character/idle/Idle_005.png',
+        'assets/images/character/idle/Idle_006.png',
+        'assets/images/character/idle/Idle_007.png',
+        'assets/images/character/idle/Idle_008.png',
+        'assets/images/character/idle/Idle_009.png',
+        'assets/images/character/idle/Idle_010.png',
+        'assets/images/character/idle/Idle_011.png'
+    ]
+
     currentImage = 0;
     world;
     speed = 10;
 
-    constructor(){
+    constructor(y){
         super();
         this.loadImage('assets/images/character/idle/Idle_000.png');
         this.loadImages(this.images_walking);
         this.loadImages(this.images_running);
         this.loadImages(this.images_sliding);
+        this.loadImages(this.images_jumping);
+        this.loadImages(this.images_idle);
         this.width = 200;
         this.height = 200;
         this.x = 50;
         this.y = 420;
+        this.applyGravity();
         this.animate();
     }
 
     animate() {
         setInterval(()=>{
             if(this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-            this.x += this.speed
-            this.otherDirection = false;
+            this.moveRight();
             } else if (this.world.keyboard.LEFT && this.x > 0) {
-            this.x -= this.speed   
-            this.otherDirection = true;
+            this.moveLeft();
             }
+
+            if(this.world.keyboard.UP && !this.isAboveGround()) {
+                this.jump();
+            }
+
             this.world.camera_x = -this.x + 50;
         }, 1000/60);
 
-        setInterval (() => {
-            if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT ) {
-            let i = this.currentImage % this.images_walking.length; // i = 0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8....
-            let path = this.images_walking[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;}
-        },50)
-
-         setInterval (() => {
-            if(this.world.keyboard.DOWN ) {
-            let i = this.currentImage % this.images_sliding.length; // i = 0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8....
-            let path = this.images_sliding[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;}
-        },50)
-
          setInterval (() => {
             if(this.world.keyboard.SHIFT && (this.world.keyboard.RIGHT || this.world.keyboard.LEFT )) {
-            let i = this.currentImage % this.images_running.length; // i = 0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8....
-            let path = this.images_running[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;}
+            this.playAnimations(this.images_running)
+            }else if(this.world.keyboard.DOWN && (this.world.keyboard.RIGHT || this.world.keyboard.LEFT )){
+            this.playAnimations(this.images_sliding)
+            }else if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT ) {
+            this.playAnimations(this.images_walking)   
+            }else if(this.world.keyboard.UP){
+            this.playAnimations(this.images_jumping);   
+            }else{
+            this.playAnimations(this.images_idle)
+            }
         },50)
 
     }
     
     jump(){
-
+      return  this.speedY = 20;
     }
 }
--- classes/movable_object.class.js
+++ classes/movable_object.class.js
@@ -6,6 +6,21 @@ class MovableObject {
     height;
     imageCache = {};
     otherDirection = false;
+    speedY = 0;
+    acceleration = 1;
+
+    applyGravity(){
+        setInterval( () => {
+            if(this.isAboveGround() || this.speedY > 0 ) {
+                this.y -=  this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25 ) ;
+    }
+
+    isAboveGround(){
+        return this.y < 420;
+    }
 
     loadImage(path) {
         this.img = new Image ();
@@ -21,11 +36,20 @@ class MovableObject {
     }
 
     moveRight() {
-        console.log("Moving right");
+            this.x += this.speed
+            this.otherDirection = false;
         
     }
 
     moveLeft(){
+        this.x -= this.speed   
+        this.otherDirection = true;
+    }
 
+    playAnimations(images) {
+        let i = this.currentImage % images.length; // i = 0,1,2,3,4,5,6,7,8,0,1,2,3,4,5,6,7,8....
+            let path = images[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
     }
 }
